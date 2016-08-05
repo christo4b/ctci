cdc30817a2686bb619fc6a71a2c4891d1d668bfa
Chris Bassano <christo4b@gmail.com>
2016-08-05 13:17:02 -0700

    palindromePermutations with a Set for storage

--- ArraysAndStrings/1.3.js
+++ ArraysAndStrings/1.3.js
@@ -1,3 +1,4 @@
+'use strict';
 // Palindrome Permutation: Given a string, write a function to check if it is a Permutation
 // of a palindrome.
 
@@ -8,11 +9,23 @@
 
 function palindromePerm(str){
 
+  if (typeof str !== 'string'){
+    return false;
+  }
   
+  let storage = new Set();
+  for (let char of str){
+    if (char !== ' '){
+      if (storage.has(char)){
+        storage.delete(char);
+      } else {
+        storage.add(char);
+      }
+    }
+  }
 
+  return storage.size <= 1;
 }
-
-
 
 
 console.log(palindromePerm("isabel belisa")); // true
